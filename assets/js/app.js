3bf216b8e9b1f24a922816ebf2f6d893b177bced
Rifars <ramlifr12@gmail.com>
2021-04-13 11:41:53 +0700

    first Commit

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+AOS.init({
+    duration: 800
+});
+
+// $(document).on("click", '[data-toggle="lightbox"]', function(event) {
+//     event.preventDefault();
+//     $(this).ekkoLightbox();
+//   });
